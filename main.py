9c7a0a0b66793ce4803ca69021b2c512f3494934
Patrick Assaf <patrickassaf96@gmail.com>
2020-05-26 12:26:22 -0700

    Parse and process data from the intents.json file

--- main.py
+++ main.py
@@ -14,4 +14,17 @@ import json
 with open("intents.json") as file:
     data = json.load(file)
 
-print(data)
+words = []
+labels = []
+docs_x = []
+docs_y = []
+
+for intent in data["intents"]:
+    for pattern in intent["patterns"]:
+        extractedWords = nltk.word_tokenize(pattern)
+        words.extend(extractedWords)
+        docs_x.append(pattern)
+        docs_y.append(intent["tag"])
+
+    if intent["tag"] not in labels:
+        labels.append(intent["tag"])
