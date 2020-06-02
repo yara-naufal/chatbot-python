4d80f74346185aa01a93eb061398289de2574691
Patrick Assaf <patrickassaf96@gmail.com>
2020-06-01 21:12:21 -0700

    Set up neural network training model

--- main.py
+++ main.py
@@ -65,3 +65,5 @@ net = tflearn.fully_connected(net, 8)
 net = tflearn.fully_connected(net, 8)
 net = tflearn.fully_connected(net, len(output[0]), activation="softmax")
 net = tflearn.regression(net)
+
+model = tflearn.DNN(net)
