72b031e666b2e86b9047441551fa937d488e5bdb
Patrick Assaf <patrickassaf96@gmail.com>
2020-06-05 21:19:38 -0700

    Display correct type of response from intents file depending on tag of user input

--- main.py
+++ main.py
@@ -100,6 +100,11 @@ def chat():
         results = model.predict([bag_of_words(inp, words)])
         results_index = numpy.argmax(results)
         tag = labels[results_index]
-        print(tag)
+        
+        for tg in data["intents"]:
+            if tg["tag"] == tag:
+                responses = tg["responses"]
+        
+        print(random.choice(responses))
 
 chat()
